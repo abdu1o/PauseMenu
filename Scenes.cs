118b47b08f049fb53a1a70ee691f6c4bbc385feb
Oleksandr Kapara <103050489+abdu1o@users.noreply.github.com>
2022-08-21 18:55:30 +0300

    Add files via upload

---
+++ Scenes.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void ChangeScenes(int numberScenes)
+    {
+        SceneManager.LoadScene(numberScenes);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
